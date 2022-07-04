09a04703f87a8f048be8cbd53ab3e6ddb21d5a66
Paurav <paurav@opphub.net>
2022-07-04 19:02:59 -0400

    updated DOM traversal

--- public/js/getInputData.js
+++ public/js/getInputData.js
@@ -1,20 +1,20 @@
 export function getInputData() {
-    var _a, _b, _c;
+    var _a, _b;
     // Output array of letters with corresponding status and position
     const letters = [];
     // Get the HTML element with user input data
-    const gameRowElements = (_c = (_b = (_a = document === null || document === void 0 ? void 0 : document.querySelector('game-app')) === null || _a === void 0 ? void 0 : _a.shadowRoot) === null || _b === void 0 ? void 0 : _b.getElementById('board')) === null || _c === void 0 ? void 0 : _c.getElementsByTagName('game-row');
+    const gameRowElements = (_b = (_a = document === null || document === void 0 ? void 0 : document.getElementById('wordle-app-game')) === null || _a === void 0 ? void 0 : _a.getElementsByTagName('div')[0]) === null || _b === void 0 ? void 0 : _b.getElementsByTagName('div')[0].children;
     for (const gameRowElement of gameRowElements || []) {
-        if (gameRowElement && gameRowElement.shadowRoot) {
+        if (gameRowElement) {
             // Get all the tile elements
-            const gameTileElements = gameRowElement.shadowRoot.querySelectorAll('div game-tile');
+            const gameTileElements = gameRowElement.querySelectorAll('div[data-state]');
             // Track position of each letter for each attempt
             let index = 0;
             // For each tile, get each letter with its status and position
             for (const gameTileElement of gameTileElements) {
                 if (gameTileElement) {
-                    const letter = gameTileElement.getAttribute('letter');
-                    const evaluation = gameTileElement.getAttribute('evaluation');
+                    const letter = gameTileElement.innerHTML;
+                    const evaluation = gameTileElement.getAttribute('data-state');
                     if (letter) {
                         // Add the letter information to the output
                         letters.push({
